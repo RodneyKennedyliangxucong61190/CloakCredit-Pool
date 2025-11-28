7bab02829ce7204d35ae1bd780711a7ea1b7626c
otpfmabtmksfrk6931CesarWolfe <dolce_petter0@icloud.com>
2025-11-28 14:18:29 +0800

    update contract addresses after redeploy

--- webapp/src/config/contracts.ts
+++ webapp/src/config/contracts.ts
@@ -1,9 +1,12 @@
 import FHELendingPoolArtifact from '../contracts/FHELendingPool.json';
+import ConfidentialWETHArtifact from '../contracts/ConfidentialWETH.json';
 
-// Contract address - will be updated after deployment
-export const LENDING_POOL_ADDRESS = '0xd674264b7A3c6927581B0786995eb44B5F94F4fC';
+// Contract addresses - deployed to Sepolia
+export const CWETH_ADDRESS = '0x8671241CAC29118F883a660aD94586F12cDBF6D6';
+export const LENDING_POOL_ADDRESS = '0xEBaf219D0bb14C243d29A3a8cCdF252482cE92E8';
 
-// Contract ABI
+// Contract ABIs
+export const CWETH_ABI = ConfidentialWETHArtifact.abi;
 export const LENDING_POOL_ABI = FHELendingPoolArtifact.abi;
 
 // Chain ID
